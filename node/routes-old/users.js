1c9c40e028aa9469feb9a96777c6908270767206
ppm1988 <ppm.career@gmail.com>
2020-02-01 17:23:17 +0530

    converted angular to dashbaord

--- node/routes-old/users.js
+++ node/routes-old/users.js
@@ -79,7 +79,7 @@ router.post('/register', (req, res) => {
 // Login
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
-    successRedirect: '/angular',
+    successRedirect: '/dashboard',
     failureRedirect: '/users/login',
     failureFlash: true
   })(req, res, next);
